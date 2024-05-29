f88a623bfdccfbb81e77d9a612b662ccab194518
Ankit Jangid <ankitjangid8501@gmail.com>
2024-05-29 22:33:32 +0530

    commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,15 @@
 import Image from "next/image";
 
 export default function Home() {
-  return (
-   <h1>Welcome to my New Project</h1>
+  return ( 
+    <>
+    <h1>Welcome to my New Project</h1>
+    <h1>Welcome to my New Project</h1>
+    <h1>Welcome to my New Project</h1>
+    <h1>Welcome to my New Project</h1>
+    <h1>Welcome to my New Project</h1>
+    <h1>Welcome to my New Project</h1>
+    <h1>Welcome to my New Project</h1>
+    </>
   );
 }
