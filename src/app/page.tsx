66702b09f53035e73bb6f1616c5ac329e8401f1b
Ankit Jangid <ankitjangid8501@gmail.com>
2024-05-31 22:09:29 +0530

    commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,14 +4,14 @@ export default function Home() {
   return ( 
     <>
     <h1>Welcome to my New Project</h1>
-    <h1>Welcome to my New Project</h1>
-    <h1>Welcome to my New Project</h1>
-    <h1>Welcome to my New Project</h1>
-    <h1>Welcome to my New Project</h1>
-    <h1>Welcome to my New Project</h1>
-    <h1>Welcome to my New Project</h1>
-    <h1>Welcome to my New Project</h1>
-    <h1>Welcome to my New Project</h1>
+    <h2>Welcome to my New Project</h2>
+    <h3>Welcome to my New Project</h3>
+    <h4>Welcome to my New Project</h4>
+    <h5>Welcome to my New Project</h5>
+    <h6>Welcome to my New Project</h6>
+    <h5>Welcome to my New Project</h5>
+    <h4>Welcome to my New Project</h4>
+    <h3>Welcome to my New Project</h3>
     <h1>Welcome to my New Project</h1>
     <h1>Welcome to my New Project</h1>
     <h1>Welcome to my New Project</h1>
