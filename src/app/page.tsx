fad5a2f3d2bba4c18f1efaae41d228418eb07418
Ankit Jangid <ankitjangid8501@gmail.com>
2024-06-03 22:48:27 +0530

    commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -42,14 +42,6 @@ export default function Home() {
     Ankit jangid 
     <h1>Welcome to my New Project</h1>
     Ankit jangid 
-    <h1>Welcome to my New Project</h1>
-    Ankit jangid 
-    <h1>Welcome to my New Project</h1>
-    Ankit jangid 
-    <h1>Welcome to my New Project</h1>
-    Ankit jangid 
-    <h1>Welcome to my New Project</h1>
-    Ankit jangid 
     </>
   );
 }
