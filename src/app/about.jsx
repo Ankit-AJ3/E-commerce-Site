6539b6df0df78417ff6f2aec1aed692ec08e9671
Ankit Jangid <ankitjangid8501@gmail.com>
2024-06-06 22:16:06 +0530

    commit

--- src/app/about.jsx
+++ src/app/about.jsx
@@ -3,9 +3,9 @@ import React from 'react'
 const about = () => {
   return (
     <div>
-      This is about section
-      This is about section
-      This is about section
+      This is about section11
+      This is about section21
+      This is about section31
     </div>
   )
 }
