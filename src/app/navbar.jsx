759571a4d181036c10f31b9d4a6653a2790e7592
Ankit Jangid <ankitjangid8501@gmail.com>
2024-06-13 21:36:32 +0530

    commit

--- src/app/navbar.jsx
+++ src/app/navbar.jsx
@@ -3,18 +3,10 @@ import React from 'react'
 const navbar = () => {
   return (
     <div>
-      This is Navbar
-      This is Navbar
-      This is Navbar
-      This is Navbar
-      This is Navbar
-      This is Navbar
-      This is Navbar<div className="ad"></div>
-      <div className="ad"></div>
-      This is Navbar
-      This is Navbar
-      div.container
-    </div>
+      <div>
+          NAVBAR
+       </div>  
+</div>
   )
 }
 
