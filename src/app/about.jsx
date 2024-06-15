6dff81c112827c824a8cd54c33ab71f99b58a5ae
Ankit Jangid <ankitjangid8501@gmail.com>
2024-06-15 18:58:04 +0530

    commit

--- src/app/about.jsx
+++ src/app/about.jsx
@@ -4,10 +4,6 @@ const about = () => {
   return (
     <div>
       This is about section11
-      This is about section21
-      This is about section21
-      This is about section31
-      This is about section31
       This is about section31
       This is about section31
     </div>
