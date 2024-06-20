e87af020d04ce10a9e625b0b8d6e53c0c29c6a65
Ankit Jangid <ankitjangid8501@gmail.com>
2024-06-20 13:45:50 +0530

    commit

--- src/app/footer.jsx
+++ src/app/footer.jsx
@@ -2,10 +2,14 @@ import React from 'react'
 
 const footer = () => {
   return (
-    <div>
-      This is About J1
-      This is About Js 
-    </div>
+    <footer className="flex flex-col justify-center gap-4 p-7 bg-black text-white text-center mt-12">
+      <p className="text-2xl font-bold flex justify-center items-center gap-2">Thankyou for the Visit <FaFireAlt className="text-yellow-400"/></p>
+      <div className="flex flex-col gap-4 sm:flex-row justify-between items-center">
+      <Socials />
+      <p>&copy; 2024 Tushar Bhatt. All rights reserved.</p>
+      </div>
+     
+    </footer>
   )
 }
 
